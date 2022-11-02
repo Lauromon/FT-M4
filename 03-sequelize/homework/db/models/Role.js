480b31325ba30e597decf72671e9a88c9ed2a6fd
Lautaro <orbesle@gmail.com>
2022-11-02 01:13:25 -0300

    tuki

--- 03-sequelize/homework/db/models/Role.js
+++ 03-sequelize/homework/db/models/Role.js
@@ -2,6 +2,12 @@ const { DataTypes } = require('sequelize');
 
 module.exports = sequelize => {
   sequelize.define('Role', {
-
+    name: {
+      type: DataTypes.STRING,
+      unique: true
+    },
+    description: {
+      type: DataTypes.STRING
+    }
   })
 }
--- 03-sequelize/homework/middlewares/character.js
+++ 03-sequelize/homework/middlewares/character.js
@@ -1,5 +1,45 @@
 const { Router } = require('express');
-const { Op, Character, Role } = require('../db');
+const { Op, Character, Role, } = require('../db');
 const router = Router();
+
+router.post('/', async(req,res)=>{
+    const {code,name,hp,mana} = req.body;
+    
+    if(!code || !name || !hp || !mana){
+    return res.status(404).send('Falta enviar datos obligatorios');
+    }  
+    try{  
+        const personaje = await Character.create(req.body);
+        res.status(201).json(personaje);
+    }catch(e){
+        res.status(404).send("Error en alguno de los datos provistos");
+    }
+});
+
+router.get('/', async(req,res)=>{
+    const {race} = req.query;
+    if(!race){
+        const todos = await Character.findAll();
+        res.json(todos);  
+    }else{
+        const raza = await Character.findAll({
+            where: {
+                race: race
+            }
+        });
+        res.json(raza);
+    }
+    
+})
+
+router.get('/:code', async(req,res)=>{
+    const {code} = req.params; 
+    const pj = await Character.findByPk(code);
+    if(pj !== null){    
+        res.json(pj);
+    }else{
+        res.status(404).send(`El código ${code} no corresponde a un personaje existente`)
+    }
+})
 
 module.exports = router;
